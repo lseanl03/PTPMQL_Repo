082750240b53db56f29efafa83a48d85f89ec95c
LSEANL03 <haov80530@gmail.com>
2025-02-17 08:11:26 +0700

    BTH_1702_BMI

--- PTPMQL/Demo_MVC/Controllers/StudentController.cs
+++ PTPMQL/Demo_MVC/Controllers/StudentController.cs
@@ -1,3 +1,4 @@
+using Demo_MVC.Models;
 using DemoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,33 @@ namespace DemoMVC.Controllers
         public IActionResult Create(Student std)
         {
             ViewBag.Message = "ID:" + std.Id + " FullName:" + std.FullName;
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult BMIResult()
+        {
+            return View();
+        }
+
+        public IActionResult BMIResult(BMI bMI)
+        {
+            if(bMI.Weight == 0 || bMI.Height == 0 || bMI.Gender == null)
+            {
+                ViewBag.Message = "Vui lòng nhập đầy đủ thông tin";
+                return View();
+            }
+            string result = "";
+            double mHeight = bMI.Height / 100; 
+            bMI.Result = (int)(bMI.Weight / (mHeight * mHeight));
+            if(bMI.Result < 18.5) result = "Bạn đang gầy";
+            else if(bMI.Result < 24.9) result = "Bạn đang bình thường";
+            else if(bMI.Result < 29.9) result = "Bạn đang thừa cân";
+            else if(bMI.Result < 34.9) result = "Bạn đang béo phì cấp độ 1";
+            else if(bMI.Result < 39.9) result = "Bạn đang béo phì cấp độ 2";
+            else ViewBag.Message = "Bạn đang béo phì cấp độ 3";
+
+            ViewBag.Message = "Giới tính: " + bMI.Gender + " Kết quả BMI: " + bMI.Result + " Đánh giá: " + result;   
             return View();
         }
     }
---
+++ PTPMQL/Demo_MVC/Models/BMI.cs
@@ -0,0 +1,10 @@
+namespace Demo_MVC.Models
+{
+    public class BMI
+    {
+        public string Gender { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public int Result { get; set; }
+    }
+}
